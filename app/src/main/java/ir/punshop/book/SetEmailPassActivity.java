e09e3474a62dfa532b981ea19b9f02162503aedc
Mahdi <mahdi.d.97.76@gmail.com>
2019-02-18 15:18:03 +0330

    nothing

---
+++ app/src/main/java/ir/punshop/book/SetEmailPassActivity.java
@@ -0,0 +1,13 @@
+package ir.punshop.book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetEmailPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_email_pass);
+    }
+}
