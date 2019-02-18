fe74dbbcd18f5037920c88ff4002e5e7ad3387b2
Mahdi <mahdi.d.97.76@gmail.com>
2019-02-18 13:48:05 +0330

    nothing

--- app/src/main/java/activities/InOrUpActivity.java
+++ app/src/main/java/activities/InOrUpActivity.java
@@ -2,15 +2,36 @@ package activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import CustomControls.CustomButton;
 import ir.punshop.book.R;
 
 public class InOrUpActivity extends AppCompatActivity {
 
+    CustomButton signIn;
+    CustomButton signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_or_up);
+
+        signIn = findViewById(R.id.sign_in);
+        signUp = findViewById(R.id.sign_up);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
