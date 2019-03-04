627972976e3ba0f08bd67bcc30528bc35377df59
mahdi.deymi <mahdi.d.97.76@gmail.com>
2019-03-04 09:48:08 -0800

    last

--- app/src/main/java/activities/SignUpActivity.java
+++ app/src/main/java/activities/SignUpActivity.java
@@ -3,16 +3,35 @@ package activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 
+import CustomControls.CustomButtonBold;
+import CustomControls.CustomEditText;
+import CustomControls.CustomTextView;
 import ir.punshop.book.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+//    CustomEditText nameFamily, telNumber , stuNumber;
+//    CustomEditText colligateUnit , feild;
+//
+//    CustomTextView nameError , telError , stuNumError , unitError , feildError;
+//
+//    CustomButtonBold signUpBtn;
+//
+//    CheckBox roleCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+//        init();
+
     }
+
+//    private void init() {
+//
+//    }
 }
